26710a6b6e9f5ad2e9961493b2a4081e35569135
Vahan Khachatryan <vahan.gti8190@mail.ru>
2019-04-15 14:14:38 +0300

    Configuration in schema.sql and data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,52 +1,70 @@
-INSERT INTO Access (id, version, login, password, name, is_active)
-VALUES (1, 0, 'Vagan18', 'vagan228', 'Vagan', true);
-
-INSERT INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
+/*
+ * Insert the data to the 'OFFICE' table.
+ */
+INSERT INTO ORGANIZATION ("id", "version", "name", "full_name", "inn", "kpp", "address", "phone", "is_active")
 VALUES (1, 0, 'Oracle', 'Oracle', '111', '111', 'street 1', '111', true);
 
-INSERT INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
+INSERT INTO ORGANIZATION ("id", "version", "name", "full_name", "inn", "kpp", "address", "phone", "is_active")
 VALUES (2, 0, 'IBM', 'International Business Machines', '222', '222', 'street 2', '222', true);
 
+/*
+ * Insert the data to the 'OFFICE' table.
+ */
+INSERT INTO OFFICE ("id", "version","name", "address", "phone", "is_active")
+VALUES (1, 0, 'OracleOffice', 'street 1', '111', true);
 
-INSERT INTO Office (id, version, org_id, name, address, phone, is_active)
-VALUES (1, 0, 1, 'OracleOffice', 'street 1', '111', true);
+INSERT INTO OFFICE ("id", "version","name", "address", "phone", "is_active")
+VALUES (2, 0,'IBMOffice', 'street 2', '222', true);
 
-INSERT INTO Office (id, version, org_id, name, address, phone, is_active)
-VALUES (2, 0, 2, 'IBMOffice', 'street 2', '222', true);
+/*
+ * Insert the data to the 'OFFICE_ORGANIZATION' table.
+ */
+INSERT INTO OFFICE_ORGANIZATION ("office_id", "organization_id","version")
+VALUES (1, 1,0);
 
-
-
-INSERT INTO Doc_Type (id, version, code, name)
-VALUES (1, 0, '03', '	Свидетельство о рождении');
-
-INSERT INTO Doc_Type (id, version, code, name)
-VALUES (2, 0, '07', 'Военный билет');
-
-
-INSERT INTO Country (id, version, code, name)
-VALUES (1, 0, '643', 'Российская Федерация');
-
-INSERT INTO Country (id, version, code, name)
-VALUES (2, 0, '051', 'Республика Армения');
+INSERT INTO OFFICE_ORGANIZATION ("office_id", "organization_id","version")
+VALUES (2, 2,0);
 
 
+/*
+ * Insert the data to the 'DOCUMENT' table.
+ */
+INSERT INTO DOCUMENT ("id", "version", "code", "name","date")
+VALUES (1, 0, '03', 'birth certificate','02.02.2000');
 
-INSERT INTO User (id, version, doc_type_id, country_id, office_id, first_name, middle_name, second_name, position,
-                  phone, doc_number, doc_date, is_identified)
-VALUES (1, 0, 1, 1, 1, 'Aleksandr', 'Aleksandrovich', 'Aleksandrov', 'TeamLead', '111', '111', '25.05.1999', true);
+INSERT INTO DOCUMENT ("id", "version", "code", "name","date")
+VALUES (2, 0, '07', 'military ID','03.03.2000');
 
-INSERT INTO User (id, version, doc_type_id, country_id, office_id, first_name, middle_name, second_name, position,
-                  phone, doc_number, doc_date, is_identified)
-VALUES (2, 0, 2, 2, 2, 'Vladimir', 'Vladimirovich', 'Vladimirov', 'Java developer', '222', '222', '20.05.1999', true);
+/*
+ * Insert the data to the 'COUNTRY' table.
+ */
+INSERT INTO COUNTRY ("id", "version", "code", "name")
+VALUES (1, 0, '643', 'Russian Federation');
 
-
-
-INSERT INTO User (id, version, doc_type_id, country_id, office_id, first_name, middle_name, second_name, position,
-                  phone, doc_number, doc_date, is_identified)
-VALUES (3, 0, 1, 1, 2, 'Viktor', 'Viktorovich', 'Viktorov', 'Project Manager', '333', '333', '18.05.1999', true);
+INSERT INTO COUNTRY ("id", "version", "code", "name")
+VALUES (2, 0, '051', 'Republic of Armenia');
 
 
+/*
+ * Insert the data to the 'USER' table.
+ */
+INSERT INTO USER("id", "version", "country_id","document_id", "office_id", "first_name", "middle_name", "second_name", "position",
+                 "phone", "is_identified")
+VALUES (1, 0, 1, 1, 1, 'Aleksandr', 'Aleksandrovich', 'Aleksandrov', 'TeamLead','111', true);
 
-INSERT INTO User (id, version, doc_type_id, country_id, office_id, first_name, middle_name, second_name, position,
-                  phone, doc_number, doc_date, is_identified)
-VALUES (4, 0, 2, 2, 1, 'Oleg', 'Olegovich', 'Olegov', 'Java developer', '333', '333', '15.05.1999', true);
+
+INSERT INTO USER("id", "version", "country_id","document_id", "office_id", "first_name", "middle_name", "second_name", "position",
+                 "phone", "is_identified")
+VALUES (2, 0, 2, 2, 2, 'Vladimir', 'Vladimirovich', 'Vladimirov', 'Java developer', '222', true);
+
+
+
+INSERT INTO USER("id", "version", "country_id","document_id", "office_id", "first_name", "middle_name", "second_name", "position",
+                 "phone", "is_identified")
+VALUES (3, 0, 1, 1, 2, 'Viktor', 'Viktorovich', 'Viktorov', 'Project Manager', '333', true);
+
+
+
+INSERT INTO USER("id", "version", "country_id","document_id", "office_id", "first_name", "middle_name", "second_name", "position",
+                 "phone", "is_identified")
+VALUES (4, 0, 2, 2, 1, 'Oleg', 'Olegovich', 'Olegov', 'Java developer', '333', true);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,87 +1,105 @@
-
-CREATE TABLE IF NOT EXISTS Access
-(
-  id        INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  version   INTEGER     NOT NULL,
-  login     VARCHAR(20) NOT NULL,
-  password  VARCHAR(20),
-  name      VARCHAR(50) NOT NULL,
-  is_active BOOLEAN     NOT NULL
-);
-
-CREATE INDEX IX_Access_Id ON Access (id);
-
+/*
+ * This is an 'ORGANIZATION' table.
+ * The table has a connection with the table 'OFFICE_ORGANIZATION'
+ * */
 CREATE TABLE IF NOT EXISTS Organization
 (
-  id        INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  version   INTEGER     NOT NULL,
-  name      VARCHAR(50) NOT NULL,
-  full_name VARCHAR(50) NOT NULL,
-  inn       VARCHAR(12) NOT NULL,
-  kpp       VARCHAR(9)  NOT NULL,
-  address   VARCHAR(50) NOT NULL,
-  phone     VARCHAR(15) NOT NULL,
-  is_active BOOLEAN     NOT NULL
+  "id"        INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  "version"   INTEGER     NOT NULL,
+  "name"      VARCHAR(50) NOT NULL,
+  "full_name" VARCHAR(50) NOT NULL,
+  "inn"       VARCHAR(12) NOT NULL,
+  "kpp"       VARCHAR(9)  NOT NULL,
+  "address"   VARCHAR(50) NOT NULL,
+  "phone"     VARCHAR(15) NOT NULL,
+  "is_active" BOOLEAN     NOT NULL
 );
 
-CREATE INDEX IX_Organization_Id ON Organization (id);
-
+CREATE INDEX IX_Organization_Id ON Organization ("id");
+/*
+ * This is an 'OFFICE' table.
+ * The table has a connection with the table 'OFFICE_ORGANIZATION'
+ * */
 CREATE TABLE IF NOT EXISTS Office
 (
-  id        INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  version   INTEGER     NOT NULL,
-  org_id    INTEGER,
-  name      VARCHAR(50) NOT NULL,
-  address   VARCHAR(50) NOT NULL,
-  phone     VARCHAR(15) NOT NULL,
-  is_active BOOLEAN     NOT NULL,
-  FOREIGN KEY (org_id) REFERENCES Organization (id)
+  "id"        INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  "version"   INTEGER     NOT NULL,
+  "name"      VARCHAR(50) NOT NULL,
+  "address"   VARCHAR(50) NOT NULL,
+  "phone"     VARCHAR(15) NOT NULL,
+  "is_active" BOOLEAN     NOT NULL,
 );
 
-CREATE INDEX IX_Office_Id ON Office (id);
-CREATE INDEX IX_Office_Org_Id ON Office (org_id);
-
-CREATE TABLE IF NOT EXISTS Doc_Type
+CREATE INDEX IX_Office_Id ON Office ("id");
+/*
+ * This is a many-to-many join table 'OFFICE_ORGANIZATION'.
+ * It connects the 'ORGANIZATION' table and 'OFFICE'
+ * */
+CREATE TABLE IF NOT EXISTS Office_Organization
 (
-  id      INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  version INTEGER     NOT NULL,
-  code    VARCHAR(3)  NOT NULL,
-  name    VARCHAR(60) NOT NULL
+  "office_id" INTEGER NOT NULL,
+  "organization_id" INTEGER NOT NULL,
+  PRIMARY KEY("office_id","organization_id"),
+  "version"   INTEGER     NOT NULL,
+  FOREIGN KEY ("office_id") REFERENCES OFFICE("id"),
+  FOREIGN KEY ("organization_id") REFERENCES ORGANIZATION("id"),
+);
+/*
+ * This is an 'Document' table.
+ * Table connects one to many  with table 'USER.document_id'
+ * */
+CREATE TABLE IF NOT EXISTS Document
+(
+  "id"      INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  "version" INTEGER     NOT NULL,
+  "code"    VARCHAR(3)  NOT NULL,
+  "name"    VARCHAR(60) NOT NULL,
+  "date"    VARCHAR(15) NOT NULL
 );
 
-CREATE INDEX IX_Doc_Type_Id ON Doc_Type (id);
-
+CREATE INDEX IX_Document_Id ON Document ("id");
+/*
+ * This is an 'COUNTRY' table.
+ * Table connects one to one with table 'USER.country_id'
+ * */
 CREATE TABLE IF NOT EXISTS Country
 (
-  id      INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  version INTEGER     NOT NULL,
-  code    VARCHAR(3)  NOT NULL,
-  name    VARCHAR(60) NOT NULL
+  "id"      INTEGER     NOT NULL PRIMARY KEY  AUTO_INCREMENT,
+  "version" INTEGER     NOT NULL,
+  "code"    VARCHAR(3)  NOT NULL,
+  "name"    VARCHAR(60) NOT NULL
 );
 
-CREATE INDEX IX_Country_Id ON Country (id);
+CREATE INDEX IX_Country_Id ON Country ("id");
 
+/*
+ * This is an 'USER' table.
+ *
+ * Table connects :
+ * many to one with table 'COUNTRY.id'
+ * many to one with table 'DOCUMENT.id'
+ * many to one with table 'OFFICE.id'
+ * */
 CREATE TABLE IF NOT EXISTS User
 (
-  id            INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  version       INTEGER     NOT NULL,
-  doc_type_id   INTEGER,
-  country_id    INTEGER,
-  office_id     INTEGER,
-  first_name    VARCHAR(50) NOT NULL,
-  middle_name   VARCHAR(50) NOT NULL,
-  second_name   VARCHAR(50) NOT NULL,
-  position      VARCHAR(20) NOT NULL,
-  phone         VARCHAR(15) NOT NULL,
-  doc_number    VARCHAR(10) NOT NULL,
-  doc_date      VARCHAR(15) NOT NULL,
-  is_identified BOOLEAN     NOT NULL,
-  FOREIGN KEY (doc_type_id) REFERENCES Doc_Type (id),
-  FOREIGN KEY (country_id) REFERENCES Country (id),
-  FOREIGN KEY (office_id) REFERENCES Office (id),
+  "id"            INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  "version"       INTEGER     NOT NULL,
+  "country_id"    INTEGER,
+  "document_id"    INTEGER,
+  "office_id"     INTEGER,
+  "first_name"    VARCHAR(50) NOT NULL,
+  "middle_name"   VARCHAR(50) NOT NULL,
+  "second_name"   VARCHAR(50) NOT NULL,
+  "position"      VARCHAR(20) NOT NULL,
+  "phone"         VARCHAR(15) NOT NULL,
+  "is_identified"  BOOLEAN     NOT NULL,
+
+  FOREIGN KEY ("office_id") REFERENCES Office ("id"),
+  FOREIGN KEY ("country_id") REFERENCES Country("id"),
+  FOREIGN KEY ("document_id") REFERENCES Document("id")
 );
 
-CREATE INDEX IX_User_Id ON User (id);
-CREATE INDEX IX_User_Doc_Type_Id ON User (doc_type_id);
-CREATE INDEX IX_User_Country_Id ON User (country_id);
-CREATE INDEX IX_User_Office_Id ON User (office_id);
+CREATE INDEX IX_User_Id ON User ("id");
+CREATE INDEX IX_User_Country_Id ON User ("country_id");
+CREATE INDEX IX_User_Document_Id ON User ("country_id");
+CREATE INDEX IX_User_Office_Id ON User ("office_id");
